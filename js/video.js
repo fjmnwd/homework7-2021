a7e6db7c6687206984aefc0b4d8e76f967a88750
Fishbaptism <yu_yang1998@126.com>
2021-03-31 23:36:03 +0800

    first version

---
+++ js/video.js
@@ -0,0 +1,62 @@
+var video = document.getElementById('player1');
+var volume = document.getElementById('volume');
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	volume.innerHTML = 100 + "%";
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.95;
+	console.log("New speed is ", video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.95;
+	console.log("New speed is ", video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 15 >= video.duration) {
+		video.currentTime = 0;
+	}
+	else video.currentTime += 15;
+	console.log("New time is ", video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		this.InnerHTML = "Unmute";
+		console.log("Unmute");
+	}
+	else {
+		video.muted = true;
+		this.InnerHTML = "Mute";
+		console.log("Mute");
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	video.volume = this.value / 100;
+	volume.innerHTML = this.value + "%";
+	console.log(video.volume);
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
